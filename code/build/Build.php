922206ab6de86c9918598222650a8dfb0679f767
Steve Wake <stephen.wake@careers.govt.nz>
2015-10-01 23:48:41 +1300

    Added dodgy way to force trait-loader to rebuild cache on /dev/build

---
+++ code/build/Build.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Dummy DataModel included to force trait_loader to regenrate trait cache on build. This may be a really dumb way
+ * to do it but it is what it is.
+ */
+
+class ModularBuildModel extends DataObject {
+	public function requireTable() {
+		DB::dontRequireTable(__CLASS__);
+	}
+	public function requireDefaultRecords() {
+		new \sgn\TraitManifest(BASE_PATH, true);
+	}
+}
