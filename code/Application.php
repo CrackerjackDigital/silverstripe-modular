e571debdc88a33eedca0f072af43f7b938800bd2
Steve Wake <stephen.wake@careers.govt.nz>
2015-12-01 12:50:13 +1300

    Added simple device_mode method, really ugly relies on DEVICE_MOBILE or DEVICE_DESKTOP being set elsewhere

--- code/Application.php
+++ code/Application.php
@@ -1,6 +1,9 @@
 <?php
 
 class ModularApp extends ModularModule {
+	const DeviceMobile = 'mobile';
+	const DeviceDesktop = 'desktop';
+
 	// base dir for loading requirements from, if not set then theme folder will be used (e.g. themes/default/)
 	private static $requirements_path;
 
@@ -10,5 +13,18 @@ class ModularApp extends ModularModule {
 	 */
 	public static function requirements_path() {
 		return static::config()->get('requirements_path') ?: SSViewer::get_theme_folder();
+	}
+
+	/**
+	 * Use DEVICE_ABC constants to figure out 'device mode' ('mobile' or 'desktop').
+	 *
+	 * @return string 'mobile' or 'desktop'
+	 */
+	public static function device_mode() {
+		if (defined('DEVICE_MOBILE')) {
+			return self::DeviceMobile;
+		} else {
+			return self::DeviceDesktop;
+		}
 	}
 }
