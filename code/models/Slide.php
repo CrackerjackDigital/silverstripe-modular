0a63bb4357ec332ae0d16e9caa933d8cab48049a
Steve Wake <steve@moveforward.co.nz>
2016-08-16 11:03:53 +1200

    Sort field on slides

--- code/models/Slide.php
+++ code/models/Slide.php
@@ -4,6 +4,9 @@ namespace Modular\Models;
 use \Modular\Model;
 
 class Slide extends Model {
+	private static $db = [
+		'Sort' => 'Int'
+	];
 	private static $has_one = [
 		'CarouselBlock' => 'Modular\Blocks\Block'
 	];
