33178103c950326af2a68d7f09cd7993f0dc49a2
Steve Wake <steve@moveforward.co.nz>
2016-08-16 10:52:21 +1200

    Checklist -> SubBlocks, HasSlides fix

--- code/relationships/HasSlides.php
+++ code/relationships/HasSlides.php
@@ -4,14 +4,19 @@ namespace Modular\Relationships;
 use \Modular\Fields\Field;
 
 class HasSlides extends Field {
+	const RelationshipName    = 'Slides';
+	const RelatedClassName    = 'Modular\Models\Slide';
+	const GridFieldConfigName = 'Modular\GridField\GridFieldConfig';
+
 	private static $has_many = [
-		'Slides' => 'Modular\Models\Slide'
+		self::RelationshipName => 'Modular\Models\Slide',
 	];
+
 	public function cmsFields() {
 		return [
 			$this->gridField(
-				'Slides'
-			)
+				static::RelationshipName
+			),
 		];
 	}
 }
