59aca68b9dd32ec86c81f5bb4f1b722b7a8f99e1
Steve Wake <steve@moveforward.co.nz>
2016-08-16 21:29:45 +1200

    Moved GridList code into modular-gridlist module

--- code/extensions/views/HasGridList.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Page model extension for pages which have a GridList component.
- */
-class HasGridExtension extends SiteTreeExtension {
-
-}
--- code/gridlist/SearchConstraints.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Modular\GridList;
-
-class SearchConstraints extends Constraints {
-	const SessionKeyPrefix = 'SCH';
-
-}
