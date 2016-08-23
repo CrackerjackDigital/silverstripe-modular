c7c1f241b5383b6a210fc52dab0a2c063538acf7
Steve Wake <steve@moveforward.co.nz>
2016-08-23 22:59:24 +1200

    Links is a HasMany

--- code/relationships/HasLinks.php
+++ code/relationships/HasLinks.php
@@ -6,7 +6,7 @@ use SS_List;
 /**
  * @method SS_List Links
  */
-class HasLinks extends HasManyMany {
+class HasLinks extends HasMany {
 	const RelationshipName = 'Links';
 	const RelatedClassName = 'Modular\Models\InternalOrExternalLink';
 	const GridFieldConfigName = 'Modular\GridField\HasLinksGridFieldConfig';
