e1804bbcaa8b20468386ede5000cdd6cb9ecaaf4
Steve Wake <wakes@digitalhod.net>
2016-12-26 20:06:39 +1300

    added HTTP interface, tweaks to code

--- code/Controller.php
+++ code/Controller.php
@@ -2,5 +2,11 @@
 namespace Modular;
 
 class Controller extends \Controller {
-
+	/**
+	 * Invoking a controller returns the controller itself.
+	 * @return $this
+	 */
+	public function __invoke() {
+		return $this;
+	}
 }
