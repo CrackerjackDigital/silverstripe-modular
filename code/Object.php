189cd6b03c5ac1bfdef8274564341eb830199d6e
Steve Wake <steve@moveforward.co.nz>
2017-04-13 13:07:48 +1200

    Force load duplication trait

--- code/Object.php
+++ code/Object.php
@@ -9,6 +9,7 @@ require_once 'traits/bitfield.php';
 require_once 'traits/cache.php';
 require_once 'traits/config.php';
 require_once 'traits/debugging.php';
+require_once 'traits/duplication.php';
 require_once 'traits/enabler.php';
 require_once 'traits/json.php';
 require_once 'traits/lang.php';
