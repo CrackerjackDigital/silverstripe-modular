971f899a3796c6e61f514830325a867e928517e9
Steve Wake <steve@moveforward.co.nz>
2017-05-04 21:49:21 +1200

    comments

--- code/traits/bitfield.php
+++ code/traits/bitfield.php
@@ -2,6 +2,14 @@
 namespace Modular\Traits;
 
 trait bitfield {
+	/**
+	 * Check if the bit field has the provided bits set to 1. All provided test bits must be set.
+	 *
+	 * @param int $bitFieldToCheck
+	 * @param int $bitsToTestAreSet
+	 *
+	 * @return bool
+	 */
 	public static function testbits($bitFieldToCheck, $bitsToTestAreSet) {
 		return $bitsToTestAreSet === ( $bitFieldToCheck & $bitsToTestAreSet);
 	}
