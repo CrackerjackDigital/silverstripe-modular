b98f570006ef8540718ac021a171653256871745
Steve Wake <steve@moveforward.co.nz>
2017-06-06 14:35:25 +1200

    CacheHash invalidate parents add invalidation task

--- code/extensions/model/CacheHash.php
+++ code/extensions/model/CacheHash.php
@@ -10,18 +10,40 @@ class CacheHash extends ModelExtension  {
 		self::CacheHashFieldName => 'Varchar(255)'
 	];
 
+	private static $cache_hash_invalidate_parents = true;
+
 	public function onBeforeWrite() {
-		if ($this()->isChanged() || !$this()->{self::CacheHashFieldName}) {
-			$this()->{self::CacheHashFieldName} = md5(microtime());
-		}
 		parent::onBeforeWrite();
+		if ($this()->isChanged()) {
+			$this->cacheHashInvalidate();
+		}
 	}
 
 	/**
-	 * Set CacheHash field to now
+	 * Set CacheHash field to now, checks config.cache_hash_invalidate_parents or first of any parameters passed.
+	 *
+	 * @throws \ValidationException
 	 */
 	public function cacheHashInvalidate() {
-		$this()->{self::CacheHashFieldName} = md5(microtime());
+		$invalidateParents = func_num_args()
+			? func_get_args()[0]
+			: $this->config()->get('cache_hash_invalidate_parents');
+
+		$hash = $this->cacheHashGenerate();
+		$this()->{self::CacheHashFieldName} = $hash;
+
+		/** @var \DataObject $parent */
+		if ($invalidateParents && $this->owner->ParentID) {
+			$parent = $this->owner->Parent();
+			if ($parent->exists() && $parent->hasExtension(self::class)) {
+				$parent->{self::CacheHashFieldName} = $hash;
+				$parent->write();
+			}
+		}
+	}
+
+	public function cacheHashGenerate() {
+		return md5( uniqid(microtime()));
 	}
 
 }
---
+++ code/tasks/CacheHashInvalidationTask.php
@@ -0,0 +1,25 @@
+<?php
+namespace Modular\Tasks;
+
+use Modular\Extensions\Model\CacheHash;
+use Modular\Task;
+
+/**
+ * CacheHashInvalidationTask update all files in the system with a new CacheHash
+ *
+ * @package Modular\Tasks
+ */
+class CacheHashInvalidationTask extends Task {
+
+	/**
+	 * Update all files in system with new cache hash.
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function execute( $params = [], &$resultMessage = '' ) {
+		static::debug_info( "Updating all File cache hashes" );
+		\DB::query( "update File set CacheHash = md5(concat(Filename, now()))" );
+		static::debug_info( \DB::affected_rows() . " updated" );
+	}
+
+}
