5fc92bf7f8dae90d8c93eedcaf41c33e5f5615fb
Steve Wake <steve@moveforward.co.nz>
2017-06-27 23:36:25 +1200

    Ref T731 - Forums, topics and posts

--- code/traits/reflection.php
+++ code/traits/reflection.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace Modular\Traits;
 
 trait reflection {
 	/**
 	 * Should return a Model (DataObject)
+	 *
 	 * @return \DataObject
 	 */
 	abstract public function __invoke();
@@ -12,21 +14,26 @@ trait reflection {
 	 * Remove namespace from a class and trim supplied from start or end of class name
 	 * e.g. Modular\Edges\SocialOrganisation -> Organisation
 	 *
-	 * @param $className
+	 * @param       $className
+	 * @param bool  $stripNamespace remove the namespace if present
+	 * @param array $trim this from the start or end of the class name
+	 *
 	 * @return string
 	 */
-	public static function name_from_class_name($className, $trim = ['Model', 'Social']) {
-		$className = static::strip_namespace($className);
-		foreach ($trim as $prefixOrSuffix) {
-			$len = strlen($prefixOrSuffix);
+	public static function name_from_class_name( $className, $stripNamespace = true, $trim = [ 'Model', 'Social' ] ) {
+		$className = $stripNamespace ? static::strip_namespace( $className ) : $className;
 
-			if (substr($className, 0, $len) == $prefixOrSuffix) {
-				$className = substr($className, $len);
+		foreach ( $trim as $prefixOrSuffix ) {
+			$len = strlen( $prefixOrSuffix );
+
+			if ( substr( $className, 0, $len ) == $prefixOrSuffix ) {
+				$className = substr( $className, $len );
 			}
-			if (substr($className, -$len) == $prefixOrSuffix) {
-				$className = substr($className, 0, -$len);
+			if ( substr( $className, - $len ) == $prefixOrSuffix ) {
+				$className = substr( $className, 0, - $len );
 			}
 		}
+
 		return $className;
 	}
 
@@ -34,20 +41,22 @@ trait reflection {
 	 * Return the class name of the passed thing
 	 *
 	 * @param Object|mixed $modelOrClassName
-	 * @param bool          $stripNamespace
+	 * @param bool         $stripNamespace
+	 *
 	 * @return string
 	 */
-	public static function derive_class_name($modelOrClassName, $stripNamespace = false) {
-		if ($modelOrClassName) {
-			$modelOrClassName = is_object($modelOrClassName) ? get_class($modelOrClassName) : $modelOrClassName;
-			if ($stripNamespace && is_array($modelOrClassName)) {
-				foreach ($modelOrClassName as &$className) {
-					$className = static::strip_namespace($className);
+	public static function derive_class_name( $modelOrClassName, $stripNamespace = false ) {
+		if ( $modelOrClassName ) {
+			$modelOrClassName = is_object( $modelOrClassName ) ? get_class( $modelOrClassName ) : $modelOrClassName;
+			if ( $stripNamespace && is_array( $modelOrClassName ) ) {
+				foreach ( $modelOrClassName as &$className ) {
+					$className = static::strip_namespace( $className );
 				}
-			} else if ($stripNamespace) {
-				$modelOrClassName = static::strip_namespace($modelOrClassName);
+			} else if ( $stripNamespace ) {
+				$modelOrClassName = static::strip_namespace( $modelOrClassName );
 			}
 		}
+
 		return $modelOrClassName;
 	}
 
@@ -56,15 +65,17 @@ trait reflection {
 	 * class or subclass of) the provided interfaceName via instanceof.
 	 *
 	 * @param string $interfaceName
+	 *
 	 * @return array of [ className => extensionInstance ]
 	 */
-	public function extensionsByInterface($interfaceName) {
+	public function extensionsByInterface( $interfaceName ) {
 		$extensions = [];
-		foreach ($this()->getExtensionInstances() as $extension) {
-			if ($extension instanceof $interfaceName) {
-				$extensions[get_class($extension)] = $extension;
+		foreach ( $this()->getExtensionInstances() as $extension ) {
+			if ( $extension instanceof $interfaceName ) {
+				$extensions[ get_class( $extension ) ] = $extension;
 			}
 		}
+
 		return $extensions;
 	}
 
@@ -74,17 +85,19 @@ trait reflection {
 	 * TODO: implement 'depthFirst' flag to get the 'leaf' classes of heirarchy first
 	 *
 	 * @param bool $excludeThisClass if true then don't include the called class in the list
+	 *
 	 * @return array with [ 'namespaced class name' => 'no-namespaced class name' ] for each subclass
 	 */
-	public static function subclasses($excludeThisClass = true) {
+	public static function subclasses( $excludeThisClass = true ) {
 		$classes = [];
 
-		foreach (\ClassInfo::subclassesFor(get_called_class()) as $className) {
-			if ($excludeThisClass && ($className == get_called_class())) {
+		foreach ( \ClassInfo::subclassesFor( get_called_class() ) as $className ) {
+			if ( $excludeThisClass && ( $className == get_called_class() ) ) {
 				continue;
 			}
 			$classes[ $className ] = $className;
 		}
+
 		return $classes;
 	}
 
@@ -92,9 +105,10 @@ trait reflection {
 	 * Return class name without namespace if there is one passed.
 	 *
 	 * @param string $maybeNamespacedClassName
+	 *
 	 * @return string
 	 */
-	public static function strip_namespace($maybeNamespacedClassName = null) {
-		return current(array_reverse(explode('\\', $maybeNamespacedClassName ?: get_called_class())));
+	public static function strip_namespace( $maybeNamespacedClassName = null ) {
+		return current( array_reverse( explode( '\\', $maybeNamespacedClassName ?: get_called_class() ) ) );
 	}
 }
