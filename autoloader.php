13f56c6091a7f3961825a7d85cbc74456d7d938d
Steve Wake <steve@moveforward.co.nz>
2018-09-06 12:55:47 +1200

    always autoload modular traits

--- autoloader.php
+++ autoloader.php
@@ -1,18 +1,14 @@
 <?php
 // if we are flushing then autoload traits from code/traits directory.
-if (isset($_REQUEST['flush'])) {
-	if (!file_exists(__DIR__ . '/_manifest_exclude')) {
-		spl_autoload_register(function ($class) {
-			$class = current(array_reverse(explode('\\', $class)));
-			if (strtolower($class) == $class) {
-				if (file_exists($path = __DIR__ . "/code/traits/$class.php")) {
-					require_once($path);
-				}
-				return;
+if (!file_exists(__DIR__ . '/_manifest_exclude')) {
+	spl_autoload_register(function ($class) {
+		$class = current(array_reverse(explode('\\', $class)));
+		if (strtolower($class) == $class) {
+			if (file_exists($path = __DIR__ . "/code/traits/$class.php")) {
+				require_once($path);
 			}
-		});
-		foreach (glob(__DIR__ . '/../*/autoloader.php') as $filePathName) {
-			require_once($filePathName);
+			return;
 		}
-	}
+	});
 }
+
